98354a94293d96256235595463fa9dcd12d4675f
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2025-10-22 17:46:13 +0330

    More embed logo stuff

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: '',
+  title: 'PAL & Emotecraft Documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
